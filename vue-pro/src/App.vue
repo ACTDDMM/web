d8c07284fc810884b973f7b3378d0416e896855b
宇宙第一帅 <2277251026@qq.com>
2022-06-13 18:21:49 +0800

    作业

--- vue-pro/src/App.vue
+++ vue-pro/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <router-view/>
+   <my-header></my-header>
+   <router-view/>
+    <my-footer></my-footer>
   </div>
 </template>
 
 <script>
+import MyFooter from './components/MyFooter.vue'
+
+import  './assets/css/style.css';
+
+import MyHeader from './components/MyHeader.vue';
   export default {
+  components: { MyFooter, MyHeader },
     
   }
+ 
 </script>
 
 <style lang="scss" scoped>
---
+++ vue-pro/src/components/MyHeader.vue
@@ -0,0 +1,73 @@
+<template>
+
+    <div class="my-header">
+        <header>
+		
+																	
+            <!-- first header -->
+		<div   class="header-top clearfix">
+			<div class="container">
+				<div class="container-left">
+					<!-- logo -->
+					<a href="">
+                        <img src="../../public/logo.png" alt="">
+                    </a>
+				</div> 
+                <div class="container-input">
+                   <input type="text" placeholder="相机">
+                    <button  class="btn">搜索</button>
+                </div>
+                <div class="container-right">
+                    <ul>
+                        <li><a href="">登录</a></li>
+                        <li><a href="">注册</a></li>
+                        <li><a href="">购物车</a></li>
+                    </ul>
+
+                </div>
+                <div class="clear"></div>
+			</div> <!--  container custom_container -->
+		</div> 
+       
+        <div class="nav" >
+            <div class="nav-container">
+            <el-menu
+
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+ 
+  text-color="#333"
+  active-text-color="#ffd04b">
+  <el-menu-item index="1">首页</el-menu-item>
+  <el-menu-item index="2" >关于我们</el-menu-item>
+  <el-menu-item index="3" >服务与支持</el-menu-item>
+  <el-menu-item index="4">新品推荐</el-menu-item>
+  <el-menu-item index="5" >关于我们</el-menu-item>
+    <el-submenu index="6">
+    <template slot="title" 	>个人中心</template>
+    <el-menu-item index="6-1" style="font-size:18px">修改个人信息</el-menu-item>
+    <el-menu-item index="6-2">个人订单查询</el-menu-item>
+ 
+  
+  </el-submenu>
+</el-menu>
+</div>
+        </div>
+																			<!-- third row -->
+
+
+	</header>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
