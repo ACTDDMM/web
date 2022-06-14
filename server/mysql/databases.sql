445f36af94186e6eaf575cc16a9e169ca0b45a04
张佳华 <13759963313@163.com>
2022-06-14 11:41:23 +0800

    sql

--- server/mysql/databases.sql
+++ server/mysql/databases.sql
@@ -1,6 +1,6 @@
 -- #设置客户端连接服务器端的编码
 
-set names utf8mb4;
+set names utf8;
 
 -- #丢弃数据库，如果存在
 
@@ -8,7 +8,7 @@ drop database if exists photography;
 
 -- #创建新的数据库，设置存储字符的编码
 
-CREATE DATABASE photography charset='utf8';
+CREATE DATABASE photography charset=utf8;
 
 -- #进入数据库
 
@@ -84,10 +84,11 @@ create table py_aboutus(
 -- 轮播图标
 
 create table py_index_carousel(
-    cid int PRIMARY KEY AUTO_INCREMENT,
-    img VARCHAR(128),
-    title VARCHAR(64),
-    href VARCHAR(128)
+    cid INT PRIMARY KEY AUTO_INCREMENT,
+    img VARCHAR(127),
+    title VARCHAR(127),
+    href VARCHAR(127),
+    camera_id INT
 );
 
 -- 首页商品表
@@ -100,10 +101,73 @@ create table py_index_product(
     camera_id INT -- 和商品表id相对应
 );
 
+-- 用户地址表
+
+create TABLE py_receiver_address(
+    aid int PRIMARY KEY auto_increment,
+    -- 用户id
+    user_id int,
+    -- 接受人姓名
+    receiver VARCHAR(16),
+    -- 省
+    province VARCHAR(16),
+    -- 市
+    city VARCHAR(16),
+    -- 县
+    county VARCHAR(16),
+    -- 详细地址
+    address VARCHAR(128),
+    -- 手机
+    cellphone VARCHAR(16),
+    -- 是否为当前用户的默认地址
+    is_default BOOLEAN
+);
+
+-- 购物车表
+
+CREATE TABLE py_shopping_cart(
+    cid int PRIMARY KEY auto_increment,
+    -- 用户编号
+    user_id int,
+    -- 商品id
+    camera_id int,
+    -- 数量
+    count int
+);
+
+-- 用户订单表
+
+CREATE TABLE py_order(
+    oid int PRIMARY KEY auto_increment,
+    user_id int,
+    addree_id int,
+    -- 订单状态  1-等待付款  2-等待发货  3-运输中  4-已签收  5-已取消
+    status INT,
+    -- 下单时间
+    order_time BIGINT,
+    -- 付款时间
+    pay_time BIGINT,
+    -- 发货时间
+    deliver_time BIGINT,
+    -- 签收时间
+    received_time BIGINT
+);
+
+-- 用户订单详情表
+
+CREATE TABLE py_order_detail(
+    did int PRIMARY KEY auto_increment,
+    -- 订单号
+    order_id int,
+    -- 商品编号
+    camera_id int,
+    -- 数量
+    count int
+);
+
 -- # 插入数据
 
 insert into py_admin values(null,'admin','123456');
-
 
 insert into py_camera_family values(1,'canon');
 
@@ -116,6 +180,8 @@ insert into py_camera_family values(4,'hasselblad');
 insert into py_camera_family values(5,'sony');
 
 insert into py_camera_family values(6,'Fujifilm');
+
+insert into py_camera_family values(7,'leica');
 
 -- #商品表
 
@@ -231,4 +297,137 @@ values(
         '10bit',
         '4K 30P',
         '4000-4500万'
+    );
+
+insert into
+    py_camera
+values(
+        null,
+        7,
+        'leica',
+        45799.00,
+        '徕卡（Leica）Q2全画幅便携数码相机 黑色19051 （4730万像素 4K视频录制 内置定焦镜头 ）',
+        '/img/product/e036819be879179c.jpg',
+        500,
+        1655096567278,
+        '徕卡Q2',
+        '数码相机',
+        '全画幅',
+        '14bit',
+        '4K 30P',
+        '3000-4000万'
+    );
+
+insert into
+    py_camera
+values(
+        null,
+        1,
+        'canon',
+        5499.00,
+        '佳能(Canon) EOS 800D单反相机 入门单反相机高清摄像佳能800D单机(不含镜头) 高级套餐(64G卡+电池+相机包+三脚架)',
+        '/img/product/a621bbee183f2f64.jpg',
+        500,
+        1655096567278,
+        '佳能(Canon) EOS 800D',
+        '单反相机',
+        'APS-C画幅',
+        '6bit',
+        '1080P 60P',
+        '3800-4500万'
+    );
+
+insert into
+    py_camera
+values(
+        null,
+        1,
+        'canon',
+        18538.00,
+        '佳能(Canon) 5d4 5D Mark IV 专业全画幅单反相机单机/套机 4K视频单反相机 EF 50mm f/1.8 STM',
+        '/img/product/d02ed404fc0eb0c4.png',
+        500,
+        1655096567278,
+        '佳能(Canon) EOS 600D',
+        '单反相机',
+        '全画幅',
+        '6bit',
+        '1080P 960P',
+        '3000-4000万'
+    );
+
+insert into
+    py_camera
+values(
+        null,
+        5,
+        'sony',
+        9499.00,
+        'SONY 索尼 DSC-RX100M7 黑卡7 RX100VII 眼部对焦 Vlog rx100M7 黑卡7(豪华套装)',
+        '/img/product/fe35fd8fc4fe06e1.jpg',
+        500,
+        1655096567278,
+        'SONY 索尼 DSC-RX100M7 黑卡7 RX100VII 眼部对焦 Vlog rx100M7 黑卡7(豪华套装)',
+        '数码相机',
+        '全画幅',
+        '6bit',
+        '4K 60P',
+        '3000-3800万'
+    );
+
+insert into
+    py_camera
+values(
+        null,
+        6,
+        'Fujifilm',
+        6499.00,
+        '富士(FUJIFILM)X-T30 II/XT30 II 微单相机 套机(15-45mm镜头 ) 银色 2610万像素 18种胶片模拟 视频提升 ',
+        '/img/product/6f48a5aadff66186.jpg',
+        500,
+        1655096567278,
+        '富士(FUJIFILM)X-T30',
+        '微单相机',
+        'APS-C',
+        '14bit',
+        '4K视频',
+        '2000-3000万'
+    );
+
+
+
+insert into
+    py_index_carousel VALUES(
+        null,
+        '/img/index/carousel/1.jpg',
+        '佳能(Canon) EOS 800D单反相机',
+        'x',
+        8
+    );
+
+insert into
+    py_index_carousel VALUES(
+        null,
+        '/img/index/carousel/2.jpg',
+        '佳能(Canon) 5d4 5D Mark IV',
+        'x',
+        9
+    );
+
+insert into
+    py_index_carousel VALUES(
+        null,
+        '/img/index/carousel/3.jpg',
+        'SONY 索尼 DSC-RX100M7',
+        'x',
+        10
+    );
+
+insert into
+    py_index_carousel VALUES(
+        null,
+        '/img/index/carousel/4.jpg',
+        '富士(FUJIFILM)X-T30',
+        'x',
+        11
     );
