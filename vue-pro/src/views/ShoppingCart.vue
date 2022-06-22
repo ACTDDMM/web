8150d42945f0813eff01d52aef57d5797fc190f7
张佳华 <13759963313@163.com>
2022-06-23 00:26:04 +0800

    购物车基本功能实现

--- vue-pro/src/views/ShoppingCart.vue
+++ vue-pro/src/views/ShoppingCart.vue
@@ -18,16 +18,16 @@
         <!-- 商品 -->
         <el-table-column label="商品" width="150">
           <template slot-scope="scope"
-            ><img :src="scope.row.img" alt=""
+            ><img :src="'/img/products/' + scope.row.c_img" alt=""
           /></template>
         </el-table-column>
         <!-- 标题 -->
-        <el-table-column prop="name" label="标题" width="150">
+        <el-table-column prop="c_name" label="标题" width="150">
         </el-table-column>
 
         <!-- 价格 -->
         <el-table-column
-          prop="price"
+          prop="c_price"
           label="价格"
           width="150"
         ></el-table-column>
@@ -36,7 +36,7 @@
           <template slot-scope="scope">
             <el-input-number
               size="mini"
-              v-model="scope.row.num"
+              v-model="scope.row.count"
               :min="1"
               label="描述文字"
             ></el-input-number>
@@ -45,7 +45,7 @@
         <!-- 金额 -->
         <el-table-column label="金额" width="150">
           <template slot-scope="scope">
-            {{ (scope.row.price * scope.row.num).toFixed(2) }}
+            {{ (scope.row.c_price * scope.row.count).toFixed(2) }}
           </template>
         </el-table-column>
         <!-- 操作 -->
@@ -67,7 +67,9 @@
         <div class="summery-right">
           <div class="right-pro">
             <span>已选购1商品</span>
-            <p>共计：<span>￥8888.00元</span></p>
+            <p>
+              共计：<span>￥{{ total }}元</span>
+            </p>
             <span>结算</span>
           </div>
         </div>
@@ -80,27 +82,25 @@
 import { mapState } from "vuex";
 import "../assets/css/shoppingcart.css";
 export default {
+  // 监听multipleSelection 和 count（监听不到） 变化 更新总价
+  watch: {
+    multipleSelection() {
+      // 遍历数据 找到 count price 累加结果 赋给total
+      let total = 0;
+      this.multipleSelection.forEach((value) => {
+        total += value.count * value.c_price;
+      });
+      this.total = total.toFixed(2);
+    },
+  },
+  mounted() {
+    this.getUserCart();
+  },
   data() {
     return {
       ...mapState(["loginUserId"]),
-      tableData: [
-        {
-          id: 1,
-          img: "/img/index/carousel/1.jpg",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-          price: "10086",
-          num: 1,
-        },
-        {
-          id: 2,
-          img: "/img/index/carousel/1.jpg",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-          price: "10086",
-          num: 1,
-        },
-      ],
+      total: "0.00",
+      tableData: [],
       multipleSelection: [],
     };
   },
@@ -108,21 +108,30 @@ export default {
   methods: {
     toggleSelection(rows) {
       if (rows) {
-        rows.forEach(row => {
+        rows.forEach((row) => {
           this.$refs.multipleTable.toggleRowSelection(row);
-          console.log(1);
         });
       } else {
         this.$refs.multipleTable.clearSelection();
-        console.log(2);
       }
     },
     handleSelectionChange(val) {
       console.log(val);
       this.multipleSelection = val;
     },
+    // 删除当前购物车项
     handleDelete(index, row) {
       console.log(index, row);
+      // 发送请求 删除当前用户点击的购物车项
+      let url = "/shopcart/delitem?cid=";
+      let params = "cid=" + row.cid;
+      this.axios.post(url, params).then((res) => {
+        console.log(res);
+        alert(res.data.msg);
+        if (res.data.code == 1) {
+          this.getUserCart();
+        }
+      });
     },
     getSummaries(param) {
       const { columns, data } = param;
@@ -132,8 +141,8 @@ export default {
           sums[index] = "总价";
           return;
         }
-        const values = data.map(item => Number(item[column.property]));
-        if (!values.every(value => isNaN(value))) {
+        const values = data.map((item) => Number(item[column.property]));
+        if (!values.every((value) => isNaN(value))) {
           sums[index] = values.reduce((prev, curr) => {
             const value = Number(curr);
             if (!isNaN(value)) {
@@ -151,9 +160,14 @@ export default {
       return sums;
     },
     // 获取当前用户的购物车列表
-    getUserCart(){
-      
-    }
+    getUserCart() {
+      let url = "/shopcart/serlist?u_id=" + this.$store.state.loginUserId;
+      // + this.$store.state.loginUserId;
+      this.axios.get(url).then((res) => {
+        console.log(res);
+        this.tableData = res.data.data;
+      });
+    },
   },
 };
 </script>
